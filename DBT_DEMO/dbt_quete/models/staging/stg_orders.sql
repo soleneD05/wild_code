d2a97b73b60e8e247bc312b69a985219cbca06d4
soleneD05 <solene.degrutere@orange.fr>
2025-07-07 17:41:53 +0200

    feat: ajout modèle stg_orders avec sources et documentation

---
+++ DBT_DEMO/dbt_quete/models/staging/stg_orders.sql
@@ -0,0 +1,13 @@
+with source as (
+    select * from {{ source('my_dbt_db', 'raw_orders') }}
+),
+renamed as (
+    select
+        id as order_id,
+        customer as customer_id,
+        ordered_at,
+        store_id
+    from source
+)
+select * from renamed
+
